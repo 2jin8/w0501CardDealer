4972fa5881b109677c75b5539920285106ee5426
2jin8 <0208cr@naver.com>
2022-10-04 15:50:03 +0900

    프로젝트 수정

--- app/src/main/java/kr/ac/kumoh/s20200062/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20200062/w0501carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20200062.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20200062.w0501carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,35 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
+        getCardName(29)
+
         binding.card1.setImageResource(R.drawable.c_10_of_spades)
     }
+
+    private fun getCardName(c: Int): String {
+        Log.i("getCardName()", c.toString())
+
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        Log.i("getCardName()", shape.toString())
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        Log.i("getCardName()", number.toString())
+
+        return "c_ace_of_spades"
+    }
+
+
 }
